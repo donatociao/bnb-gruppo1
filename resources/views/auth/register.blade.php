53c47b2aaf4dde2b28fd71815b695ccf48c178d4
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-08-29 18:56:17 +0200

    1. Piccolo ritocco grafico RF1 2. Impostazione Pre RF2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,34 @@
                         @csrf
 
                         <div class="form-group row">
+                          <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address (*)') }}</label>
+
+                          <div class="col-md-6">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                          </div>
+                        </div>
+
+                        <div class="form-group row">
+                          <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password (*)') }}</label>
+
+                          <div class="col-md-6">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                              <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                          </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
@@ -24,47 +52,18 @@
                                 @enderror
                             </div>
                         </div>
-                        
+
                         <div class="form-group row">
                             <label for="surname" class="col-md-4 col-form-label text-md-right">Cognome</label>
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="surname" value="{{ old('surname') }}">
+                                <input id="surname" type="text" class="form-control" name="surname" value="{{ old('surname') }}">
                             </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="birth" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
                             <div class="col-md-6">
-                                <input id="name" type="date" class="form-control" name="birth" value="{{ old('birth') }}">
-                            </div>
-                        </div>
-
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                <input id="birth" type="date" class="form-control" name="birth" value="{{ old('birth') }}">
                             </div>
                         </div>
 
@@ -74,6 +73,13 @@
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
+                        </div>
+
+                        <div class="form-group row">
+                          <small class="col-md-6 offset-md-4">
+                            Required fields are marked with an asterisk (*).
+                          </small>
+
                         </div>
 
                         <div class="form-group row mb-0">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,9 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <a href="{{route('apartments.index')}}">Visualizza i tuoi appartamenti</a>
                 </div>
+
             </div>
         </div>
     </div>
