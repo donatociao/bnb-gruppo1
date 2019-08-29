1c07c96171f685bf59555e0fadaad51ec32dc9fa
danielevitale <danvitale@outlook.it>
2019-08-29 16:28:43 +0200

    Fine RF1

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -24,6 +24,21 @@
                                 @enderror
                             </div>
                         </div>
+                        
+                        <div class="form-group row">
+                            <label for="surname" class="col-md-4 col-form-label text-md-right">Cognome</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="surname" value="{{ old('surname') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="birth" class="col-md-4 col-form-label text-md-right">{{ __('Data di nascita') }}</label>
+                            <div class="col-md-6">
+                                <input id="name" type="date" class="form-control" name="birth" value="{{ old('birth') }}">
+                            </div>
+                        </div>
+
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
