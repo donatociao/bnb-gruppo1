194c3139c0972b8c0e7fb5376904b6f62fa690ae
veropra <prainoveronica@gmail.com>
2019-09-09 23:43:43 +0200

    Searchbar in homepage ok. Responsive ok.

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -31,12 +31,27 @@
       <div class="searchbar-container">
         <div class="title">
           <p>BoolBnB<p>
-            <div class="searchbar">
-              <form class="form my-2 my-lg-0 w-50 mx-auto">
-                <input class="form-control" type="search" placeholder="Cerca appartamenti" aria-label="Search">
-                <button class="btn btn-dark my-2 my-sm-0" type="submit">CERCA!</button>
-              </form>
-            </div>
+
+            <div class="row justify-content-center">
+                        <div class="col-12 col-md-10 col-lg-8">
+                                <div class="card-body-b row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <i class="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col">
+                                        <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Cerca l'appartamento...">
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col-auto">
+                                        <button class="btn btn-dark" type="submit">Cerca</button>
+                                    </div>
+                                    <!--end of col-->
+                                </div>
+
+                        </div>
+                        <!--end of col-->
+                    </div>
         </div>
       </div>
   <!--HEADER-->
@@ -47,10 +62,10 @@
             <p>I nostri appartamenti in evidenza:</p>
             <!--appartamenti in evidenza primi 4 (da vedere) : MORBIDO!-->
             @foreach ($appartamenti as $appartamento)
-              <div class="container-card col-md-3">
+              <div class="container-card col-md-3 col-xs-4">
                 <div class="card-body">
                   <p class="card-title">{{ $appartamento->title }}</p>
-                  <img class="dim-img col-md-12" src="{{ asset('storage/'. $appartamento->url_img) }}" alt=" " class="rounded">
+                  <img class="dim-img" alt="Responsive image" src="{{ asset('storage/'. $appartamento->url_img) }}" alt=" " class="rounded">
                 </div>
               </div>
             @endforeach
@@ -70,29 +85,27 @@
     </div>
     <!--FOOTER-->
     <div class="footer-container">
-        <div class="row">
-          <div class="col-md-12">
-            <p class="privacy">Condizioni e privacy:</p>
-            <hr class="clearfix w-100 d-md-none pb-3">
-            <div class="list">
-              <ul class="list-unstyled">
-                <li>
-                  <a href="">Condizioni</a>
-                </li>
-                <li>
-                  <a href="">Opportunità di lavoro</a>
-                </li>
-                <li>
-                  <a href="">Aiuto</a>
-                </li>
-                <li>
-                  <a href="">News</a>
-                </li>
-              </ul>
-            </div>
-            <div class="copyright">© 2019 BoolBnB, Inc. All rights reserved.</div>
-          </div>
+      <div class="col-md-12">
+        <p class="privacy">Condizioni e privacy:</p>
+        <hr class="clearfix w-100 d-md-none pb-3">
+        <div class="list">
+          <ul class="list-unstyled">
+            <li>
+              <a href="">Condizioni</a>
+            </li>
+            <li>
+              <a href="">Opportunità di lavoro</a>
+            </li>
+            <li>
+              <a href="">Aiuto</a>
+            </li>
+            <li>
+              <a href="">News</a>
+            </li>
+          </ul>
         </div>
+        <div class="copyright">© 2019 BoolBnB, Inc. All rights reserved.</div>
+      </div>
     </div>
   </div>
   </body>
