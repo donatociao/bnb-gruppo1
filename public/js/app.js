d8da4b36cb1709c5823d546cf4c084004551d838
veropra <prainoveronica@gmail.com>
2019-09-14 12:48:57 +0200

    Mod mockup 2. Ancora da modificare

--- public/js/app.js
+++ public/js/app.js
@@ -49327,8 +49327,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\booleanProjects\boolbnb-gruppo1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\booleanProjects\boolbnb-gruppo1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\bnb-gruppo1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\bnb-gruppo1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
